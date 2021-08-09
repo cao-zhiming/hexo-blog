872653d8943ca19c0144a4d83121443d413cb330
曹智铭 <xiaocao162020@xiaocao.onmicrosoft.com>
2021-08-09 22:11:40 +0800

    Update sw.js

--- source/sw.js
+++ source/sw.js
@@ -1,2 +1,2 @@
-‘use strict’;
-importScripts(‘sw-toolbox.js’); toolbox.precache([“index.html”,”style/style.css”]); toolbox.router.get(‘/images/*’, toolbox.cacheFirst); toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(["index.html","style/style.css"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
