fd01660cf95420a451103c44aafe2883d388a6ef
曹智铭 <xiaocao162020@xiaocao.onmicrosoft.com>
2021-08-10 20:39:28 +0800

    Update sw.js

--- source/sw.js
+++ source/sw.js
@@ -1,2 +1,2 @@
 'use strict';
-importScripts('https://blog.caozhiming.tk/sw-toolbox.js'); toolbox.precache(["index.html"]); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+importScripts('https://blog.caozhiming.tk/sw-toolbox.js'); toolbox.precache(["/","exam-time","covid-self-check.html"]); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
